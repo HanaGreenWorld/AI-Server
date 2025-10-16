0e420d3499ceed0061eb7f460070a35c295735a6
Jimin0304 <pjws0304@naver.com>
2025-10-17 00:34:11 +0900

    chore: host 형식 틀린거 수정

--- ai_server/settings.py
+++ ai_server/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-change-this-in-production'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', '*.run.app']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', '.run.app']
 
 # Application definition
 INSTALLED_APPS = [
